9857defed65230783d455df0113cdbde5cb61a48
Yuliya Zozulya <djulia@ukr.net>
2023-01-27 00:21:34 +0200

    adds multer logics

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const gavatar = require("gravatar");
 const { httpError } = require("../../helpers");
 const { User } = require("../../models/users");
 
@@ -8,6 +9,12 @@ async function register(req, res, next) {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     const savedUser = await User.create({ email, password: hashedPassword });
+    const avatarURL = await gavatar.url(email, {
+      s: "200",
+      r: "pg",
+      d: "404",
+    });
+    await User.findOneAndUpdate({ email }, { avatarURL }, { new: true });
     return res.status(201).json({ user: { email, id: savedUser._id } });
   } catch (error) {
     if (error.code === 11000) {
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
 const userRouter = express.Router();
 const {
   register,
@@ -10,6 +12,16 @@ const {
 const { validateUser, auth } = require("../../middleWares");
 const { userSchema, subscriptionSchema } = require("../../schema");
 const { tryCatcher } = require("../../helpers");
+const { path } = require("../../app");
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, "tmp"),
+  filename: function (req, file, cb) {
+    cb(null, Math.random() + file.originalname);
+  },
+});
+
+const upload = upload.multer({ storage });
 
 userRouter.post("/signup", validateUser(userSchema), tryCatcher(register));
 userRouter.post("/login", validateUser(userSchema), tryCatcher(login));
@@ -17,6 +29,12 @@ userRouter.get("/logout", tryCatcher(auth), tryCatcher(logout));
 userRouter.get("/current", tryCatcher(auth), tryCatcher(current));
 userRouter.patch(
   "/",
+  tryCatcher(auth),
+  validateUser(subscriptionSchema),
+  tryCatcher(subscriptionUpdate)
+);
+userRouter.patch(
+  "/avatars",
   tryCatcher(auth),
   validateUser(subscriptionSchema),
   tryCatcher(subscriptionUpdate)
